616162baee3a5f0c63269d7d636a2b0bc981986a
张瑞超 <359599907@qq.com>
2016-12-14 22:02:04 +0800

    SWIFT初始化

---
+++ PROJECT-SWIFT/PROJECT-SWIFT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PROJECT-SWIFT
+//
+//  Created by 张瑞超 on 2016/12/14.
+//  Copyright © 2016年 张瑞超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
